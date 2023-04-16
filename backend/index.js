f3cf459a615e79061da9379e2c187d1a39077b80
Andrez23 <marincarlosandres42@gmail.com>
2023-04-16 11:01:54 -0600

    organizando backend

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,23 @@
-const express= require("express");
+const express= require("express");/* */ 
 const morgan = require("morgan");
 const cors = require("cors");
 
-const app = express();
+const app = express();/* hereda todo de express, es decir, todo lo que viene*/ 
 
-app.set("Port",4000);
+app.set("Port",4000); /*morgan nos sirve para saber que tipo de peticiones está recibiedno nuestro servidor*/
 
 app.use(morgan("dev"));
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.use(express.json()); /*nos sirve apra convertir los datos a objetos json y leerlos */
 
-app.use(cors({origin: "*"}));
+app.use(cors({origin: "*"})); /*Cors nos sirve para permitir conxoines desde cualqueier cliente */
 
-app.listen(app.get("Port"),() => {console.log("Servidor corriendo por puerto", app.get("Port"));});
+/*rutas o vistas */
+
+app.listen(app.get("Port"),() => {
+    console.log("Servidor corriendo por puerto", app.get("Port"));
+});
 
 
 
