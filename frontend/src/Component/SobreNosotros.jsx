627c25a669ba6c9448d86a594e8e30c23b9cd5d4
marincarlosandres42@gmail.com <marincarlosandres42@gmail.com>
2023-06-21 15:48:25 -0500

    haciendo CRUD desde 0

--- frontend/src/Component/SobreNosotros.jsx
+++ frontend/src/Component/SobreNosotros.jsx
@@ -1,139 +1,237 @@
-import React from "react";
-import { useState } from "react";
-import uuid from "react-uuid";
+import axios from "axios"
+import React, { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 import "../Css/SobreNosotros.css"
 
 export const SobreNosotros = () => {
-  const [tasks, setTasks] = useState([]);
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState ("");
-  const [edit, setEdit] = useState(false);
+    const [clientes, setClientes] = useState([])
+    const [documento_usuario,setDocumento_usuario] = useState("")
+    const [nombre,setNombre] = useState("")
+    const [apellido,setApellido] = useState("")
+    const [telefono,setTelefono] = useState("")
+    const [direccion,setDireccion] = useState("")
+    const [correoElectronio,setCorreoElectronico] = useState("")
+    const [númeroAuxiliar,setNúmeroAuxiliar] = useState("")
+    const [fechaNacimiento,setFechaNacimiento] = useState("")
+    const[edit,setEdit]= useState(false) /*para poder editar registros */
 
-  const addTask = () => {
-    const task = {id: uuid(), title, description};
-    setTasks([...tasks,task]);
-    clear();
-  };
+    useEffect(()=>{ 
+        getData()
+    },[]);
 
-  const updateTask = () => {
-    const id = localStorage.getItem("id");
-    const newTask = { id, title, description };
-    const newTasks = tasks.map((item) => (item.id === id ? newTask : item));
-    setTasks(newTasks);
-    clear();
-  };
+    const cleanData=()=>{
+        setDocumento_usuario("")
+        setNombre("")
+        setApellido("")
+        setTelefono("")
+        setDireccion("")
+        setCorreoElectronico("")
+        setNúmeroAuxiliar("")
+        setFechaNacimiento("")
+        setEdit(false)
+    }
 
-  const actions = (e) => {
-    e.preventDefault();
-    edit ? updateTask() : addTask();
-  };
+    const getClienteByid=async(id)=>{
+        try {
+            
+        } catch (error) {
+            if(!error.response.data.ok){
+                return alert(error.response.data.message)
+            }
+            console.log("error en getUserByid", error.message) 
+        }
+    }
 
-  const deleteTasks = (id) => {
-    Swal.fire({
-      title: 'Estas seguro de Eliminar?',
-      text: "si continuas No podrás revertir el proceso!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: '¡Sí, eliminalo!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        const newTasks = tasks.filter((task) => task.id !== id);
-        setTasks(newTasks);
-        Swal.fire({
-          title:"Eliminado!",
-          text:"Sus datos han sido eliminados.",
-          icon:"success",
-          showConfirmButton: false,
-          timer: 1500
-        
-        })
-      }
-    })
-  };
+    const getData= async()=>{
+        const {data} = await axios.get('http://localhost:4000/api/cliente/list') 
+    console.log(data.clientes);
+    setClientes(data.clientes)
+    }
 
-  const getData = (id) => {
-    const task = tasks.find((item) => item.id === id); 
-    localStorage.setItem("id", id);
-    setTitle(task.title);
-    setDescription(task.description);
-    setEdit(true);
-  };
+    const saveClientes = async()=>{   /*Guardar clientes en la tabla */
+        try {
+            const newUser={
+                documento_usuario,
+                nombre,
+                apellido,
+                telefono,
+                direccion,
+                correoElectronio,
+                númeroAuxiliar,
+                fechaNacimiento 
+            }
+            await axios.post('http://localhost:4000/api/cliente/add',newUser)
+            cleanData()
+            getData()
+        } catch (error) {
+            if(!error.response.data.ok){
+               return alert(error.response.data.message)
+            }
+            console.log("error en saveClientes", error.message)       
+        }
+    };
 
-  const clear = () => {
-    setTitle("");
-    setDescription("");
-    setEdit(false);
-    localStorage.removeItem("id");
-  };
+    const updateCliente= async()=>{  /*Nos permite llamar a la funcion de actulizar clientes */
+        try {
+            const id= localStorage.getItem('id')
+            const newUser={
+                documento_usuario,
+                nombre,
+                apellido,
+                telefono,
+                direccion,
+                correoElectronio,
+                númeroAuxiliar,
+                fechaNacimiento 
+            }
+            const {data}= await axios.put("http://localhost:4000/api/cliente/update/" + id, newUser)
+            cleanData()
+            getData()
+            Swal.fire({  /*Alerta */
+                icon: 'success',
+                title: data.message,
+                showConfirmButton: false,
+                timer: 1500
+              })
+        } catch (error) {
+            if(!error.response.data.ok){
+                return alert(error.response.data.message)
+             }
+             console.log("error en updateClientes", error.message)   
+        }
+    }
 
-  return(
-    <div className="contenedor">
-      <div className="uno">
-        <div className="dos">
-          <div className="tres">
-            <h3 className="cuatro">CRUD</h3>
-            <div className="crud-contenedor">
-              <form onSubmit={actions}>
-                <div className="mb-3">
-                  <input
-                    type="text"
-                    placeholder="Título"
-                    value={title}
-                    required
-                    autoFocus
-                    className="form-control"
-                    onChange={(e) => setTitle(e.target.value)}
-                  />
+    const actions= (e)=> {
+        e.preventDefault();
+        /* saveClientes();  asi se llama las funciones, con todas es lo mismo*/
+        edit?updateCliente(): saveClientes();
+
+    };
+
+    const completeDataFields=(item)=>{  /*Con esto podemos editar nuestros registros */
+        setEdit(true)  /*Esta llamada nos permite cambiar de falso a verdadero */
+        setDocumento_usuario(item.documento_usuario)
+        setNombre(item.nombre)
+        setApellido(item.apellido)
+        setTelefono(item.telefono)
+        setDireccion(item.direccion)
+        setCorreoElectronico(item.correo_electronico)
+        setNúmeroAuxiliar(item.número_auxiliar)
+        setFechaNacimiento(item.fechaNacimiento)
+        localStorage.setItem("id", item._id) /*guardamos id en local storage*/
+    }
+
+    const deteleClientes=async(id)=>{   /*funcionalidad al boton de borrar */
+        try {
+            Swal.fire({  /*Esta es la alerta para saber si estamos seguros de borrar el registro */
+                title: 'Estás seguro?',
+                text: "No podrás revertír el proceso!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+              }).then(async(result) => {
+                if (result.isConfirmed) {
+                    const {data}=await axios.delete("http://localhost:4000/api/cliente/delete/"+ id)  /*con esta direccion borramos el usuario */
+                    Swal.fire({
+                        icon: 'success',
+                        title: data.message,
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
+              })
+        } catch (error) {
+            if(!error.response.data.ok){
+                return alert(error.response.data.message)
+             }
+             console.log("error en deleteClientes", error.message)     
+        }
+    }
+
+    return (
+        <div className="container">
+            {/*Inicio formulario */}
+            <div className="container_1">
+                <div className="container_2">
+                    <div className="card">
+                        <h1 className="crud">CRUD</h1>
+                        <div className="body">
+                            <div className="form_content">
+                                <form onSubmit={actions}>
+                                    <div>
+                                        <input type="text" placeholder="Documento" className="form-control" value={documento_usuario} required onChange={e=>setDocumento_usuario(e.target.value)}/> 
+                                    </div>
+                                    <div>
+                                        <input type="text" placeholder="Nombre" className="form-control" value={nombre} required onChange={e=>setNombre(e.target.value)} /> 
+                                    </div>
+                                    <div>
+                                        <input type="text" placeholder="Apellido" className="form-control" value={apellido} required onChange={e=>setApellido(e.target.value)} /> 
+                                    </div>
+                                    <div>
+                                        <input type="text" placeholder="Teléfono" className="form-control" value={telefono} required onChange={e=>setTelefono(e.target.value)} /> 
+                                    </div>
+                                    <div>
+                                        <input type="text" placeholder="Direccion" className="form-control" value={direccion} required onChange={e=>setDireccion(e.target.value)} /> 
+                                    </div>
+                                    <div>
+                                        <input type="text" placeholder="Correo" className="form-control" value={correoElectronio} required onChange={e=>setCorreoElectronico(e.target.value)} /> 
+                                    </div>
+                                    <div>
+                                        <input type="text" placeholder="NumAuxiliar" className="form-control" value={númeroAuxiliar} required onChange={e=>setNúmeroAuxiliar(e.target.value)} /> 
+                                    </div>
+                                    <div>
+                                        <input type="text" placeholder="FechaNacimiento" className="form-control" value={fechaNacimiento} required onChange={e=>setFechaNacimiento(e.target.value)} /> 
+                                    </div>
+                                   
+                                    <button className="btn_form " type="submit">Enviar</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="mb-3">
-                  <input
-                    type="text"
-                    placeholder="Descripción"
-                    value={description}
-                    required
-                    className="form-control"
-                    onChange={(e) => setDescription(e.target.value)}
-                  />
-                </div>
-                <button className="btn btn-primary form-control" type="submit">
-                  Save
-                </button>
-              </form>
             </div>
-          </div>
+            {/*Final formulario */}
+            {/*Inicio Tabla para guardar registros */}
+            <table className="table_info">
+                <thead className="table_info2">
+                    <tr>
+                        <th>#</th>
+                        <th>Documento</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Telefono</th>
+                        <th>Direccion</th>
+                        <th>Correo</th>
+                        <th>NumAuxiliar</th>
+                        <th>FechaNacimiento</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        clientes.map((item,i)=>(
+                            <tr key={item._id}>
+                                <td>{i+1}</td>
+                                <td>{item.documento_usuario}</td>
+                                <td onClick={()=>getClienteByid(item._id)}>{item.nombre}</td>
+                                <td>{item.apellido}</td>
+                                <td>{item.telefono}</td>
+                                <td>{item.direccion}</td>
+                                <td>{item.correoElectronio}</td>
+                                <td>{item.númeroAuxiliar}</td>
+                                <td>{item.fechaNacimiento}</td>
+                                <td>
+                                    <i className=" btn_D fas fa-trash" onClick={()=>deteleClientes(item._id)}></i> {/*Asi estamos llamando al boton para poder borrar el usuario, despues de dar click*/}
+                                    <i className=" btn_W fas fa-edit" onClick={()=>completeDataFields(item)}></i>{/*Boton de editar */}
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+            {/*Final Tabla para guardar registros */}
         </div>
-      </div>
-
-      <ul className="list-group list-group-numbered">
-          {tasks.map((task) => (
-            <li
-              className="list-group-item d-flex justify-content-between aling-item-start"
-              key={task.id}
-            >
-              <div className="ms-2 me-auto">
-                <div className="fw-bold">{task.title}</div>
-                {task.description}
-              </div>
-              <button
-                className="btn btn-danger me-2"
-                onClick={() => deleteTasks(task.id)}
-              >
-                <i className="fas fa-trash"></i>
-              </button>
-              <button
-                className="btn btn-warning"
-                onClick={() => getData(task.id)}
-              >
-                <i className="fas fa-edit"></i>
-              </button>
-            </li>
-          ))}
-        </ul>
-    </div>
-  )
-}
-
-
+    );
+};
